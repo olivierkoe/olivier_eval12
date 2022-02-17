2cc1bfc2b58f0a235e806ecaf82ca1a56db90a09
olivierkoe <olikoen@gmail.com>
2022-02-17 21:30:01 +0100

    code commenté

--- eval24.js
+++ eval24.js
@@ -17,54 +17,54 @@
 // possibles !
 // Je vous attends dans la prochaine session !
 
-const choiceNumber = ["C'est le juste prix ","C'est moins cher !!","C'est plus cher ","Entrez un chiffre entre 0 et 1000"];
+const choiceNumber = ["C'est le juste prix ","C'est moins cher !!","C'est plus cher ","Entrez un chiffre entre 0 et 1000"];// je cree un tbleau avec les differente reponses
 
-chiffreAleatoir = Math.floor(Math.random() * 1000);
+chiffreAleatoir = Math.floor(Math.random() * 1000);// la je definis un chiffre aleatoire
 console.log(chiffreAleatoir);
 
-let butondemarer = document.querySelector(".bouton1");
-let paragraphSelector2 = document.querySelector(".paragraphe2");
-let paragraphSelector1 = document.getElementById("in");
+let butondemarer = document.querySelector(".bouton1");// je selection mon bouton dans le html et je le stock dans une variable
+let paragraphSelector2 = document.querySelector(".paragraphe2");//je selection le pargraphe dans le html ou je vais afficher les messages
+let paragraphSelector1 = document.getElementById("in");// je récupère le contenue rentrer par le joueur que je stock dans une variable. 
 
-let tentative = 1;
+let tentative = 1;// je demarre le comptage des click du bouton
 
-butondemarer.addEventListener("click", displayMessage);
+butondemarer.addEventListener("click", displayMessage);//je définis ce qu'il se passe lorsque le joueur click sur le bouton 
 
 
-
+// la fonction qui gere le displaymessage
 function displayMessage (){ 
     if (paragraphSelector1.value < 0 || paragraphSelector1.value > 1000 || paragraphSelector1.value == isNaN()){
-        paragraphSelector2.textContent = choiceNumber[3];
+        paragraphSelector2.textContent = choiceNumber[3];//je definis que ce que le joueur rentre comme information est bien un chiffre et qu'il est bien compris entre 0 et 1000
     }
     else{
         if (paragraphSelector1.value == chiffreAleatoir){
                 if (tentative == 1){
                     paragraphSelector2.textContent = choiceNumber[0] + "C'etais votre "+ tentative + " ère tentative";
-                    tentative = tentative + 1;
+                    tentative = tentative + 1;// je compare le chiffre rentrer par le joueur et retourne le message correspondant et j'ajoute une tentative au compteur.
                 }
                 else{
                     paragraphSelector2.textContent = choiceNumber[0] + "C'etais votre "+ tentative + " em tentatives";
-                    tentative = tentative + 1;
+                    tentative = tentative + 1;// je compare le chiffre rentrer par le joueur et retourne le message correspondant et j'ajoute une tentative au compteur.
                 }
         }
         else if (paragraphSelector1.value > chiffreAleatoir){
                 if (tentative == 1){
                     paragraphSelector2.textContent = choiceNumber[1] + "C'etais votre "+ tentative + " ère tentative";
-                    tentative = tentative + 1;
+                    tentative = tentative + 1;// je compare le chiffre rentrer par le joueur et retourne le message correspondant et j'ajoute une tentative au compteur.
                 }
                 else{
                     paragraphSelector2.textContent = choiceNumber[1] + "C'etais votre "+ tentative + " em tentatives";
-                    tentative = tentative + 1;
+                    tentative = tentative + 1;// je compare le chiffre rentrer par le joueur et retourne le message correspondant et j'ajoute une tentative au compteur.
                 }
         }
         else if (paragraphSelector1.value < chiffreAleatoir){
                 if (tentative == 1){
                     paragraphSelector2.textContent = choiceNumber[2] + "C'etais votre "+ tentative + " ère tentative";
-                    tentative = tentative + 1;
+                    tentative = tentative + 1;// je compare le chiffre rentrer par le joueur et retourne le message correspondant et j'ajoute une tentative au compteur.
                 }
                 else{
                     paragraphSelector2.textContent = choiceNumber[2] + "C'etais votre "+ tentative + " em tentatives";
-                    tentative = tentative + 1;
+                    tentative = tentative + 1;// je compare le chiffre rentrer par le joueur et retourne le message correspondant et j'ajoute une tentative au compteur.
                 }
 
         }
